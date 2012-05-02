51a1bea8c3599d40805a187cb649a3147c817bdb
Wil Everts <your@cousinwil.com>
2012-05-02 00:38:48 -0700

    Initial commit - hello basic rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SpokeGeek::Application.config.secret_token = 'e2740ced90c26703bb2c90a2c5f701aceea95d7dcc6cf6b51dbceb6086647ec3dd9f152caef881015a0255b46b5f7e176bfc1aed5d0962bdffb50c24b280af3e'

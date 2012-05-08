e20cd936f4f9ad105f4791378860eff2ace9f4f7
Wil Everts <your@cousinwil.com>
2012-05-07 18:34:17 -0700

    making the title helper less brittle

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+ def title_text(page_title)                           # Method definition
+    base_title = "Spoke Geek"
+    if !page_title || page_title.empty? || page_title == 'Home'      # Boolean test
+      "#{base_title} Bicycle Club"                    # Implicit return
+    else
+      "#{base_title} | #{page_title}"                 # String interpolation
+    end
+  end
 end

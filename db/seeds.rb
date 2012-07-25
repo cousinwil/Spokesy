50925a4e8008c455c0f3ebc4a0b641a0dd6433f7
Wil Everts <your@cousinwil.com>
2012-07-25 00:21:15 -0700

    updating the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ Event.create([
 	},
     { date: "8/4/2012",
 	  description: "Join us 8/4 for the 50th Marin Century and Mt. Tam Double! We have club members representing in the double and the metric centuries!!",
-	  miles: 40,
-	  name: "Saturday Spandex"
+	  miles: 100,
+	  name: "Marin Century"
 	}])
 
 Member.create([
